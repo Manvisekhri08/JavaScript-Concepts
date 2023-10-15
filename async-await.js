cbc09f1b39f52df36f8290b6c543e45c1ee8044d
ManviSekhri <ManviSekhri08@gmail.com>
2023-10-15 11:59:12 +0530

    async/await using setTimeOut

--- async-await.js
+++ async-await.js
@@ -1,63 +1,65 @@
-//what is async?
-// keyword that is used before a function to create a async function
-// what is async function?
+/* what is async?
+keyword that is used before a function to create a async function
+what is async function?
 
 
 const promise = new Promise((resolve, reject) => {
     resolve("Promise resolved value");
 })
 
-// //always return a promise
-// async function getData() {
-//     //return "IT WILL ALWAYS RETURN A PROMISE";
-//     return promise; // Promise resolved value
-// };
+//always return a promise
+async function getData() {
+    //return "IT WILL ALWAYS RETURN A PROMISE";
+    return promise; // Promise resolved value
+};
 
-// const dataPromise = getData();
+const dataPromise = getData();
 
-// // console.log(dataPromise);   ///Promise { 'IT WILL ALWAYS RETURN A PROMISE' }
+// console.log(dataPromise);   ///Promise { 'IT WILL ALWAYS RETURN A PROMISE' }
 
-// dataPromise.then((res) => console.log(res)); // Promise resolved value
+dataPromise.then((res) => console.log(res)); // Promise resolved value
 
-// this async function will always return a promise or if it doesn't return promise eg if it returns some string then it will wrap the value inside the promise and return it.
+this async function will always return a promise or if it doesn't return promise eg if it returns some string then it will wrap the value inside the promise and return it.
 
-// using await with async
-// async and await combo is used to handle promises.
+using await with async
+async and await combo is used to handle promises.
 
-// how we used to handle promises before async await
-// function getData() {
-//     promise.then(res => console.log(res));
-// }
-// getData(); //Promise resolved value
+how we used to handle promises before async await
+function getData() {
+    promise.then(res => console.log(res));
+}
+getData(); //Promise resolved value
 
-// how we handle promises using async await
-// async function handlePromise() {
-//     const value = await promise; // you will write await in front of the promise that has been resolved
-//     console.log(value); // Promise resolved value
-// }
-// handlePromise();
+how we handle promises using async await
+async function handlePromise() {
+    const value = await promise; // you will write await in front of the promise that has been resolved
+    console.log(value); // Promise resolved value
+}
+handlePromise();
 
-//await is a keyword that is only used inside your async function.
+await is a keyword that is only used inside your async function.
 
 
-// difference b/w normally handling promise and handling async await
-// why async await is important
+difference b/w normally handling promise and handling async await
+why async await is important
 
-//EXAMPLE :- 1
-// const promiseSetTimeOut = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         resolve("Promise resolved value using SetTimeOut");        // our promise will only resolve after 10 seconds 
-//     },10000)
-// }) 
+EXAMPLE :- 1
+const promiseSetTimeOut = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Promise resolved value using SetTimeOut");        // our promise will only resolve after 10 seconds 
+    },10000)
+}) 
 
-// const promiseSetTimeOut2 = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         resolve("Promise resolved value using SetTimeOut 2");        // our promise will only resolve after 5 seconds
-//     },5000)
-// }) 
+const promiseSetTimeOut2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Promise resolved value using SetTimeOut 2");        // our promise will only resolve after 5 seconds
+    },5000)
+}) */
+
+// **************************************************************************************************************************************************************************************************
 
 // EXAMPLE :-2
-const promiseSetTimeOut = new Promise((resolve, reject) => {
+/*const promiseSetTimeOut = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve("Promise resolved value using SetTimeOut");        // our promise will only resolve after 5 seconds 
     },5000)
@@ -79,31 +81,53 @@ async function handleAsyncPromise() {
     const result2 = await promiseSetTimeOut2;
     console.log("Async Promise 2");
     console.log(result2);         
-}
-//handleAsyncPromise(); // first Promise resolved value using SetTimeOut then Async Promise will be printed
-//EXAMPLE :- 1 OUTPUT
-//handleAsyncPromise(); // as this was resolved in 5 seconds still js waited for 10 sec then printed 1 first then this. both promises got resolved at same time so result2 waas printed immediately // even if we created 2 different promises they got resolved at same time.
+} 
+handleAsyncPromise(); */
 
-//EXAMPLE 2 :- OUTPUT
+/*handleAsyncPromise(); // first Promise resolved value using SetTimeOut then Async Promise will be printed
+EXAMPLE :- 1 OUTPUT
+handleAsyncPromise(); // as this was resolved in 5 seconds still js waited for 10 sec then printed 1 first then this. both promises got resolved at same time so result2 waas printed immediately // even if we created 2 different promises they got resolved at same time.
+
+EXAMPLE 2 :- OUTPUT
 handleAsyncPromise(); // now the after 5 seconds our first promise is resolved and give us the output and then after 10 seconds second promise will get resolved and give us the output
 
-//how async/await works behind the scenes
-// First its empty
-// as soon as handleAsyncPromise function is called it sees that there are 2 promises to be resolved that are taking their own time to be resolved
-// CALL STACK
-// as soon as handleAsyncPromise function is called it will push inside the call stack
-// JS synchronous single threaded language it starts executing code line by line it will first console Before await
-// then it will move further and it sees there is await promiseSetTimeOut now guess 1 is do the handleAsyncPromise() stay in stack wait for promise to be resolved?
-// no it will not, in fact the execution of handleAsyncPromise()  will get suspended and pops out for the time and it will wait till the promiseSetTimeOut is resolved then it will move ahead. promiseSetTimeOut will be resolved after 5 sec.
-// as call stack got empty after 5 sec handleAsyncPromise() again push inside and it will continue where it left from
-// then it sees another promise and check if it is resolved but 2nd promise will be resolved after 10 secs so again it will be suspended and wait for it to be resolved and handleAsyncPromise() push in again and continur the execution from where it left
+how async/await works behind the scenes
+First its empty
+Examnple 2
+as soon as handleAsyncPromise function is called it sees that there are 2 promises to be resolved that are taking their own time to be resolved
+CALL STACK
+as soon as handleAsyncPromise function is called it will push inside the call stack
+JS synchronous single threaded language it starts executing code line by line it will first console Before await
+then it will move further and it sees there is await promiseSetTimeOut now guess 1 is do the handleAsyncPromise() stay in stack wait for promise to be resolved?
+no it will not, in fact the execution of handleAsyncPromise()  will get suspended and pops out for the time and it will wait till the promiseSetTimeOut is resolved then it will move ahead. promiseSetTimeOut will be resolved after 5 sec.
+as call stack got empty after 5 sec handleAsyncPromise() again push inside and it will continue where it left from
+then it sees another promise and check if it is resolved but 2nd promise will be resolved after 10 secs so again it will be suspended and wait for it to be resolved and handleAsyncPromise() push in again and continur the execution from where it left
 
-//with example 2
 
-// function handleNormalPromise() {
-//     //js engine will not wait for promise to be resolved, it will move over to the next line.
-//     // when promise is resolved after 10 secs, then it will be printed
-//     promiseSetTimeOut.then((res) => console.log(res));
-//     console.log("Normal Promise");
-// }
-// handleNormalPromise(); // first Normal Promise will be printed then after 10 seconds Promise resolved value using SetTimeOut will be printed
+Example 1
+CALL STACK
+now the first promise is 10 secs
+so handleAsyncPromise() will move out of call stack and it will wait first promise to be resolved and then after resolved it will pushed in.
+after 10 secs it will be printed
+when handleAsyncPromise() moved to second promise and it will see second promise is already resolved and immediately print it.
+
+
+
+with example 2
+plain promise handling
+function handleNormalPromise() {
+    //js engine will not wait for promise to be resolved, it will move over to the next line.
+    // when promise is resolved after 10 secs, then it will be printed
+    promiseSetTimeOut.then((res) => console.log(res));
+    console.log("Normal Promise");
+}
+handleNormalPromise(); // first Normal Promise will be printed then after 10 seconds Promise resolved value using SetTimeOut will be printed */
+
+// **************************************************************************************************************************************************************************************************
+
+// Example 3 :- without Promise setTimeout , using fetch 
+const 
+async function handlePromise() {
+
+}
+
